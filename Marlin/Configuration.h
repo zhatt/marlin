4c5c919ab708fad02213effec45a32d71d412cb7
Craig Robson <craig@zhatt.com>
2017-03-12 22:12:30 -0700

    Change Z axis steps to 394.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -486,7 +486,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 334, 95.8}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 394, 95.8}
 
 /**
  * Default Max Feed Rate (mm/s)
